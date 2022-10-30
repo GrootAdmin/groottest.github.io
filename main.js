69fcb013821f631926d91abeb07afd380b00ac42
GrootAdmin <117033652+GrootAdmin@users.noreply.github.com>
2022-10-30 19:18:50 +0200

    index

---
+++ main.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" charset="utf-8">
+      (function(d, w, s) {
+          var widgetHash = 'cCxvHO5PsMtcm4es2b7G', bch = d.createElement(s); bch.type = 'text/javascript'; bch.async = true;
+          bch.src = '//widgets.binotel.com/chat/widgets/' + widgetHash + '.js';
+          var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(bch, sn);
+      })(document, window, 'script');
+      </script>
